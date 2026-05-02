7abfa93260488afca6cc9f4f44f27572f5c3f19a
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-05-02 22:11:54 +0530

    feat: add routing for student and educator pages with new components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/student/Home'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/student/Home";
+import CoursesList from "./pages/student/CoursesList";
+import CourseDetails from "./pages/student/CourseDetails";
+import MyEntrollments from "./pages/student/MyEntrollments";
+import Player from "./pages/student/Player";
+import Loading from "./pages/student/Loading";
+import Educator from "./pages/educator/Educator";
+import Dashboard from "./pages/educator/Dashboard";
+import AddCourse from "./pages/educator/AddCourse";
+import MyCourses from "./pages/educator/MyCourses";
+import StudentsEntrolled from "./pages/educator/StudentsEntrolled";
 
 const App = () => {
   return (
@@ -8,9 +18,21 @@ const App = () => {
       <Routes>
         {/* Define your routes here */}
         <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-entrollments" element={<MyEntrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="student-entrolled" element={<StudentsEntrolled />} />
+          <Route />
+        </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
