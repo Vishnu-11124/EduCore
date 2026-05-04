ec779227995ac4e8dfdb691255a7f20bec2e4003
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-05-04 10:52:08 +0530

    feat: integrate Clerk authentication; add publishable key and update Navbar for user sign-in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import { useClerk, UserButton, useUser } from "@clerk/react";
 
 const Navbar = () => {
-  return (
-    <div className="flex items-center justify-between px-6 py-3 border-b border-gray-200 bg-white">
-      <div>
-        <h1 className="text-xl font-semibold text-gray-900">EduCore</h1>
-      </div>
-      <div>
-        <button className="text-sm font-medium text-white bg-emerald-600 px-4 py-2 rounded-lg hover:bg-emerald-700 transition-colors">
+
+  const { openSignIn } = useClerk()
+  const { user } = useUser()
+
+return (
+  <div className="flex items-center justify-between px-6 py-3 border-b border-gray-200 bg-white">
+    <h1 className="text-xl font-semibold text-gray-900">EduCore</h1>
+
+    {
+      user ? (
+        <UserButton />
+      ) : (
+        <button
+          onClick={() => openSignIn()}
+          className="text-sm font-medium text-white bg-emerald-600 px-4 py-2 rounded-lg hover:bg-emerald-700"
+        >
           Create Account
         </button>
-      </div>
-    </div>
-  )
+      )
+    }
+  </div>
+)
+
 }
 
 export default Navbar
