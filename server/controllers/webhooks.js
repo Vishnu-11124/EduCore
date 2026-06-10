6087a9d3446ee6a9694f978b04f6e70899980e63
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-06-10 11:20:11 +0530

    refactor: simplify clerk webhooks handler by removing asyncHandler and restructuring payload verification

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,56 +1,52 @@
 import { Webhook } from "svix";
 import User from "../models/User.js";
-import asyncHandler from "../utils/asyncHandler.js";
 
-export const clerkWebhooks = asyncHandler(async (req, res) => {
+export const clerkWebhooks = async (req, res) => {
   const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-  await whook.verify(JSON.stringify(req.body), {
+  const payload = req.body;
+
+  const headers = {
     "svix-id": req.headers["svix-id"],
     "svix-timestamp": req.headers["svix-timestamp"],
     "svix-signature": req.headers["svix-signature"],
-  });
+  };
 
-  const { data, type } = req.body;
+  let event;
+
+  try {
+    event = whook.verify(payload, headers);
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid webhook signature" });
+  }
+
+  const { data, type } = event;
 
   switch (type) {
     case "user.created": {
-      const userData = {
+      await User.create({
         _id: data.id,
         name: `${data.first_name || ""} ${data.last_name || ""}`,
         email: data.email_addresses[0].email_address,
         imageUrl: data.image_url,
-      };
-
-      await User.create(userData);
-
+      });
       break;
     }
 
     case "user.updated": {
-      const userData = {
+      await User.findByIdAndUpdate(data.id, {
         name: `${data.first_name || ""} ${data.last_name || ""}`,
         email: data.email_addresses[0].email_address,
         imageUrl: data.image_url,
-      };
-
-      await User.findByIdAndUpdate(data.id, userData);
-
+      });
       break;
     }
 
     case "user.deleted": {
       await User.findByIdAndDelete(data.id);
-
       break;
     }
-
-    default:
-      break;
   }
 
-  return res.json({
-    success: true,
-    message: "Webhook processed",
-  });
-});
+  return res.json({ success: true });
+};
