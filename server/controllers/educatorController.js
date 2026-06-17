baef84d881f3015e665fdaf17d4c45bc60308d39
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-06-17 21:10:25 +0530

    feat: implement addCourse functionality with error handling and image upload

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,7 @@
-import { clerkClient, getAuth } from "@clerk/express";
+import { clerkClient } from "@clerk/express";
+import Course from "../models/Course";
+import { v2 as cloudinary } from "cloudinary";
+
 
 // update role
 export const updateRoleToEducator = async (req, res) => {
@@ -19,5 +22,40 @@ export const updateRoleToEducator = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   
+  }
+}
+
+// add neww course
+export const addCourse = async (req, res) => {
+  try {
+    const { courseData } = req.body
+    if(!courseData){
+      return res.json({ error: "Course data is required" })
+    }
+
+    const imageFile = req.file
+    if(!imageFile){
+      return res.json({ error: "Image file is required" })
+    }
+
+    const educatorId = req.auth().userId
+    if(!educatorId){
+      return res.json({ error: "Educator ID is required" })
+    }
+
+    const parsedCourseData = await JSON.parse(courseData)
+    parsedCourseData.educator = educatorId
+
+    const newCourse = await Course.create(parsedCourseData)
+
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+    newCourse.courseThumbnail = imageUpload.secure_url
+
+    await newCourse.save()
+
+    res.status(200).json({ success: true, message: "Course added successfully" })
+
+  } catch (error) {
+    res.status(500).json({ error: error.message })
   }
 }
